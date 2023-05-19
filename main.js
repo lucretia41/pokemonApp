5b02050e15b4047e5a53dc220c6e1d65459e9811
Ian Scott Martin <ianscottmartin@gmail.com>
2023-05-19 13:18:05 -0600

    simplify js

--- main.js
+++ main.js
@@ -1,29 +1,46 @@
-console.log("hello")
-
-
 const fetchPokemon = () => {
-    // console.log('fetching pokemon'); checking to see works
-    const url = `https://pokeapi.co/api/v2/pokemon/1`;
-    fetch(url)
-        .then(res => {
-            (res.json);
-            return res.json();
-        })
 
-        .then((data) => {
-            console.log(data)
-            const pokemon = {
-                name: data.name,
-                id: data.id,
-                image: data.sprites['front_default'],
-                type: data.types.map((type) => type.type.name).join
-                    (', ')
-            };
-            console.log(pokemon)
-        });
-
+    console.log("hello")
+    const promises = [];
+    for (let i = 1; i <= 150; i++) {
+        const url = `https://pokeapi.co/api/v2/pokemon/${i}`;
+        promises.push(fetch(url).then((res) => res.json()));
+    }
+    Promise.all(promises).then((results) => {
+        const pokemon = results.map((data) => ({
+            name: data.name,
+            id: data.id,
+            image: data.sprites['front_default'],
+            type: data.types.map((type) => type.type.name).join(', ')
+        }));
+        console.log(pokemon);
+    });
 };
-
 fetchPokemon();
+
+// const fetchPokemon = () => {
+//     // console.log('fetching pokemon'); checking to see works
+//     const url = `https://pokeapi.co/api/v2/pokemon/1`;
+//     fetch(url)
+//         .then(res => {
+//             (res.json);
+//             return res.json();
+//         })
+
+//         .then((data) => {
+//             console.log(data)
+//             const pokemon = {
+//                 name: data.name,
+//                 id: data.id,
+//                 image: data.sprites['front_default'],
+//                 type: data.types.map((type) => type.type.name).join
+//                     (', ')
+//             };
+//             console.log(pokemon)
+//         });
+
+// };
+
+// fetchPokemon();
 
 
