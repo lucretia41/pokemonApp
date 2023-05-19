b434238bae7a7f882157ed534026f1c7cb36b7d0
Ian Scott Martin <ianscottmartin@gmail.com>
2023-05-19 13:23:52 -0600

    checking pokemon objectarray

--- main.js
+++ main.js
@@ -13,34 +13,19 @@ const fetchPokemon = () => {
             image: data.sprites['front_default'],
             type: data.types.map((type) => type.type.name).join(', ')
         }));
-        console.log(pokemon);
+        // console.log(pokemon);
+        displayPokemon(pokemon);
+
     });
 };
+
+
+
+const displayPokemon = (pokemon) => {
+    console.log(pokemon);
+};
+
+
+
 fetchPokemon();
-
-// const fetchPokemon = () => {
-//     // console.log('fetching pokemon'); checking to see works
-//     const url = `https://pokeapi.co/api/v2/pokemon/1`;
-//     fetch(url)
-//         .then(res => {
-//             (res.json);
-//             return res.json();
-//         })
-
-//         .then((data) => {
-//             console.log(data)
-//             const pokemon = {
-//                 name: data.name,
-//                 id: data.id,
-//                 image: data.sprites['front_default'],
-//                 type: data.types.map((type) => type.type.name).join
-//                     (', ')
-//             };
-//             console.log(pokemon)
-//         });
-
-// };
-
-// fetchPokemon();
-
 
